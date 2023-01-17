238972e3610ce4f70893b594225b2f04a639390c
Karine Brandelli <karinebrandelli@live.com>
2023-01-17 14:53:30 -0300

    build: create a component for the footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+export function Footer() {
+  return (
+    <footer>
+      <p>
+        <span class="amount"> </span>
+        tasks
+      </p>
+      <p class="clear">Clear</p>
+    </footer>
+  );
+}
