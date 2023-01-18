3a1f5c20e2ede242845de600357df363da6f0c8b
Karine Brandelli <karinebrandelli@live.com>
2023-01-18 17:01:31 -0300

    docs: comment div

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -2,23 +2,15 @@ import "./Task.css";
 
 export function Task() {
   return (
-    <div class="todo-list">
-      <div class="list-items">
-        <span class="todo-task">
-          <i class="ph-circle"></i>
-          {/* <i class="ph-check-circle"></i> */}
-          <p class="task-content">tarefa</p>
+    <div className="todo-list">
+      {/* <div className="list-items">
+        <span className="todo-task">
+          <i className="ph-circle"></i>
+          <i className="ph-check-circle"></i>
+          <p className="task-content">tarefa</p>
         </span>
-        <i class="ph-x"></i>
-      </div>
-      <div class="list-items">
-        <span class="todo-task">
-          <i class="ph-circle"></i>
-          {/* <i class="ph-check-circle"></i> */}
-          <p class="task-content">tarefa</p>
-        </span>
-        <i class="ph-x"></i>
-      </div>
+        <i className="ph-x"></i>
+      </div> */}
     </div>
   );
 }
