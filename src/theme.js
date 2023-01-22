27bf246f940c21a4c05d2f8b6d17a018cb77fc5f
Karine Brandelli <karinebrandelli@live.com>
2023-01-22 15:23:29 -0300

    build: set colors to light and dark theme

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  body: 'hsl(0, 0%, 98%)',
+  text: 'hsl(236, 8%, 41%)',
+  lists: 'hsl(236, 33%, 92%)',
+  header: 'src/components/Header/assets/bg-desktop-light.jpg'
+}
+
+export const darkTheme = {
+  body: 'hsl(235, 21%, 11%)',
+  text: 'hsl(0, 0%, 98%)',
+  lists: 'hsl(235, 24%, 19%)',
+  header: 'src/components/Header/assets/bg-desktop-dark.jpg'
+}
