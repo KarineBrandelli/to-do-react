9ffc3151efed85b6c9df35306a2272b1c3e87eec
Karine Brandelli <karinebrandelli@live.com>
2023-01-22 15:24:31 -0300

    feat: implement toggle functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
-import { Title } from "./components/Title/Title";
-import { Header } from "./components/Header/Header";
-import { NewTask } from "./components/NewTask/NewTask";
+import React, { useState } from "react";
 import "./App.css";
 
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./theme";
+import { GlobalStyles } from "./global";
+
+import { Header } from "./components/Header/Header";
+import { NewTask } from "./components/NewTask/NewTask";
+
 export function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+
   return (
-    <>
-      <Header />
-      <div className="container">
-        <Title />
-        <NewTask />
-      </div>
-    </>
-  )
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <div className="container">
+          <div className="title">
+            <h1>TODO</h1>
+
+            <div className="toggle-switch">
+              <label>
+                <input
+                  className="toggle"
+                  type="checkbox"
+                />
+                <span className="slider" onClick={toggleTheme}></span>
+              </label>
+            </div>
+          </div>
+          <NewTask />
+        </div>
+      </>
+    </ThemeProvider>
+  );
 }
