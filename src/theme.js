8d331ba46ed1641f4ed5793560010775690cf10d
Karine Brandelli <karinebrandelli@live.com>
2023-01-25 15:52:25 -0300

    fix: fix header image src

--- src/theme.js
+++ src/theme.js
@@ -1,13 +1,16 @@
+import HeaderLight from './components/Header/assets/bg-desktop-light.jpg'
+import HeaderDark from './components/Header/assets/bg-desktop-dark.jpg'
+
 export const lightTheme = {
   body: 'hsl(0, 0%, 98%)',
   text: 'hsl(236, 8%, 41%)',
   lists: 'hsl(236, 33%, 92%)',
-  header: 'src/components/Header/assets/bg-desktop-light.jpg'
+  header: `${HeaderLight}`
 }
 
 export const darkTheme = {
   body: 'hsl(235, 21%, 11%)',
   text: 'hsl(0, 0%, 98%)',
   lists: 'hsl(235, 24%, 19%)',
-  header: 'src/components/Header/assets/bg-desktop-dark.jpg'
+  header: `${HeaderDark}`
 }
